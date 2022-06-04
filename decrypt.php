ca5971fb4372a49c275051eb740eb7802f14f901
Liam <78104591+Liam-Nothing@users.noreply.github.com>
2022-06-04 16:37:39 +0200

    [Feature] Add api and errors to decrypt

--- decrypt.php
+++ decrypt.php
@@ -1,4 +1,5 @@
 <?php
+
 //Convert string to binary
 function toBin($str)
 {
@@ -22,54 +23,86 @@ function toString($str)
 	return $newstring;
 }
 
-$src = 'paysage01.png'; //Change this to the image to decrypt
-
-$img = imagecreatefrompng($src); //Returns image identifier
-$real_message = ''; //Empty variable to store our message
-
-$count = 0; //Wil be used to check our last char
-$pixelX = 0; //Start pixel x coordinates
-$pixelY = 0; //start pixel y coordinates
-
-list($width, $height, $type, $attr) = getimagesize($src); //get image size
-
-for ($x = 0; $x < ($width * $height); $x++) { //Loop through pixel by pixel
-	// if($pixelX === $width+1){ //If this is true, we've reached the end of the row of pixels, start on next row
-	if ($pixelX === $width) { //If this is true, we've reached the end of the row of pixels, start on next row
-		$pixelY++;
-		$pixelX = 0;
-	}
-
-	if ($pixelY === $height && $pixelX === $width) { //Check if we reached the end of our file
-		echo ('Max Reached');
-		die();
-	}
-
-	$rgb = imagecolorat($img, $pixelX, $pixelY); //Color of the pixel at the x and y positions
-	$r = ($rgb >> 16) & 0xFF; //returns red value for example int(119)
-	$g = ($rgb >> 8) & 0xFF; //^^ but green
-	$b = $rgb & 0xFF; //^^ but blue
-
-	$blue = toBin($b); //Convert our blue to binary
-
-	$real_message .= $blue[strlen($blue) - 1]; //Ad the lsb to our binary result
-
-	$count++; //Coun that a digit was added
-
-	if ($count == 8) { //Every time we hit 8 new digits, check the value
-		if (toString(substr($real_message, -8)) === '|') { //Whats the value of the last 8 digits?
-			// echo ('done<br>'); //Yes we're done now
-			$real_message = toString(substr($real_message, 0, -8)); //convert to string and remove /
-			// echo ('Result: ');
-			header('Content-Type: application/json');
-			echo $real_message; //Show
-			// $json_string = json_encode($real_message, JSON_PRETTY_PRINT);
-			// echo "<br>";
-			// echo $json_string;
-			die;
-		}
-		$count = 0; //Reset counter
-	}
-
-	$pixelX++; //Change x coordinates to next
+function is_clean($string)
+{
+	return !preg_match("/[^. a-z\d_-]/i", $string);
 }
+
+function decryptSteno($src)
+{
+	if (is_clean($src)) {
+		if (!file_exists($src)) {
+			$return_data["type"] = "error";
+			$return_data["message"] = "File doesn't exist.";
+			return $return_data;
+		}
+	} else {
+		$return_data["type"] = "error";
+		$return_data["message"] = "Filename may only contain alphanumeric characters or numbers.";
+		return $return_data;
+	}
+
+	$img = imagecreatefrompng($src); //Returns image identifier
+	$real_message = ''; //Empty variable to store our message
+
+	$count = 0; //Wil be used to check our last char
+	$pixelX = 0; //Start pixel x coordinates
+	$pixelY = 0; //start pixel y coordinates
+
+	list($width, $height, $type, $attr) = getimagesize($src); //get image size
+
+	for ($x = 0; $x < ($width * $height); $x++) { //Loop through pixel by pixel
+		// if($pixelX === $width+1){ //If this is true, we've reached the end of the row of pixels, start on next row
+		if ($pixelX === $width) { //If this is true, we've reached the end of the row of pixels, start on next row
+			$pixelY++;
+			$pixelX = 0;
+		}
+
+		if ($pixelY === $height && $pixelX === $width) { //Check if we reached the end of our file
+			echo ('Max Reached');
+			die();
+		}
+
+		$rgb = imagecolorat($img, $pixelX, $pixelY); //Color of the pixel at the x and y positions
+		$r = ($rgb >> 16) & 0xFF; //returns red value for example int(119)
+		$g = ($rgb >> 8) & 0xFF; //^^ but green
+		$b = $rgb & 0xFF; //^^ but blue
+
+		$blue = toBin($b); //Convert our blue to binary
+
+		$real_message .= $blue[strlen($blue) - 1]; //Ad the lsb to our binary result
+
+		$count++; //Coun that a digit was added
+
+		if ($count == 8) { //Every time we hit 8 new digits, check the value
+			if (toString(substr($real_message, -8)) === '|') { //Whats the value of the last 8 digits?
+				$real_message = toString(substr($real_message, 0, -8)); //convert to string and remove /
+
+				$return_data["type"] = "success";
+				$return_data["message"] = "Success decrypting.";
+				$return_data["content"] = json_decode($real_message);
+
+				return $return_data;
+			}
+			$count = 0; //Reset counter
+		}
+
+		$pixelX++; //Change x coordinates to next
+	}
+}
+
+$return_data = [
+	"type" => null,
+	"message" => null
+];
+
+if (isset($_GET['file'])) {
+	$file = htmlspecialchars($_GET['file']);
+	$return_data = decryptSteno($file);
+} else {
+	$return_data["type"] = "error";
+	$return_data["message"] = "Enter a file name please.";
+}
+
+header('Content-Type: application/json');
+echo json_encode($return_data);
